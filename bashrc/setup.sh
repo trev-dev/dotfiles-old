97fb56d93b71d0ce255e7c3e233f6c7b8e9dce19
Trevor Richards <trev@trevdev.ca>
2021-04-25 15:42:10 -0700

    add quick setup script

---
+++ bashrc/setup.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+cd ~/.dotfiles/bashrc
+ln -s $(pwd)/.aliases $HOME/aliases
+
+ln -s $(pwd)/.bash_profile $HOME/.bash_profile
+
+ln -s $(pwd)/.bash_prompt $HOME/.bash_prompt
+
+ln -s $(pwd)/.bashrc $HOME/.bashrc
+
+source $HOME/.bashrc
