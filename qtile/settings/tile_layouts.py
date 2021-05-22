5acb9b7b982f2881188aa229637f242d78b711df
Trevor Richards <trev@trevdev.ca>
2021-05-22 16:54:27 -0700

    rework layouts

--- qtile/settings/tile_layouts.py
+++ qtile/settings/tile_layouts.py
@@ -3,14 +3,9 @@ from settings.theme import colors
 
 layouts = [
     layout.Max(),
-    layout.Stack(
-        num_stacks=2,
-        margin=3,
-        border_focus=colors['black'],
-        border_width=2),
     layout.MonadTall(margin=3, border_focus=colors['black']),
     layout.MonadWide(margin=3, border_focus=colors['black']),
-    layout.Tile(margin=3, border_focus=colors['black'], border_width=2)
+    layout.RatioTile(border_focus=colors['black'])
 ]
 
 float_settings = layout.Floating(float_rules=[
